09f7d02fe58f9bcea0e00cf531dd42387bf87fe9
Aaron <amgwlee93@gmail.com>
2016-06-24 14:44:14 -0400

    Initialization
    -The bare bones, default imported assets/models/scripts/etc.
    -Can look around, walk around, and hit the ball with hands

---
+++ Assets/Scripts/PickupParent.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent(typeof(SteamVR_TrackedObject))]
+public class PickupParent : MonoBehaviour {
+
+
+	SteamVR_TrackedObject trackedObj;
+
+	// Use this for initialization regardless if obj is enabled
+	void Awake () {
+		trackedObj = GetComponent<SteamVR_TrackedObject> ();
+	}
+	
+	// FixedUpdate is called every physics step
+	void FixedUpdate () {
+		SteamVR_Controller.Device device = SteamVR_Controller.Input ((int)trackedObj.index);
+		if (device.GetTouch (SteamVR_Controller.ButtonMask.Trigger)) {
+			Debug.Log ("You are gently holding down the Trigger");
+		}
+
+		if (device.GetTouchDown (SteamVR_Controller.ButtonMask.Trigger)) {
+			Debug.Log ("You are clicking the Trigger");
+		}
+
+		if (device.GetTouchUp (SteamVR_Controller.ButtonMask.Trigger)) {
+			Debug.Log ("You let go of the Trigger");
+		}
+
+	}
+}
