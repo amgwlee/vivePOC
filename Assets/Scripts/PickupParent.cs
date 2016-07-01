29bc63ca92f7d4d7230bcf83d409fcb5337a81e7
Aaron <amgwlee93@gmail.com>
2016-07-01 11:09:14 -0400

    Every Time We Touch
    -Added Collison detection to controllers/sphere
    -Sphere transform parented to controller on pickup
    -Velocity and angular momentum retained on drop event (aka, throwing
    works)
    - Pressing touchpad resets transform, velocity, and angular velocity of
    sphere all back to (0, 0, 0)
    -Added debug logging for every hold, press, and release button event
    (except menu and system buttons because they're different)
    -SteamVR_Unity_Toolkit imported from github (not in use yet)

--- Assets/Scripts/PickupParent.cs
+++ Assets/Scripts/PickupParent.cs
@@ -7,6 +7,9 @@ public class PickupParent : MonoBehaviour {
 
 
 	SteamVR_TrackedObject trackedObj;
+	SteamVR_Controller.Device device;
+
+	public Transform sphere;
 
 	// Use this for initialization regardless if obj is enabled
 	void Awake () {
@@ -15,18 +18,73 @@ public class PickupParent : MonoBehaviour {
 	
 	// FixedUpdate is called every physics step
 	void FixedUpdate () {
-		SteamVR_Controller.Device device = SteamVR_Controller.Input ((int)trackedObj.index);
+		device = SteamVR_Controller.Input ((int)trackedObj.index);
+
 		if (device.GetTouch (SteamVR_Controller.ButtonMask.Trigger)) {
-			Debug.Log ("You are gently holding down the Trigger");
+			Debug.Log ("You are holding the Trigger");
 		}
 
 		if (device.GetTouchDown (SteamVR_Controller.ButtonMask.Trigger)) {
-			Debug.Log ("You are clicking the Trigger");
+			Debug.Log ("You pressed down the Trigger");
 		}
 
 		if (device.GetTouchUp (SteamVR_Controller.ButtonMask.Trigger)) {
 			Debug.Log ("You let go of the Trigger");
 		}
 
+		if (device.GetPress (SteamVR_Controller.ButtonMask.Grip)) {
+			Debug.Log ("You are holding the Grip");
+		}
+
+		if (device.GetPressDown (SteamVR_Controller.ButtonMask.Grip)) {
+			Debug.Log ("You pressed down the Grip");
+		}
+
+		if (device.GetPressUp (SteamVR_Controller.ButtonMask.Grip)) {
+			Debug.Log ("You let go of the Grip");
+		}
+			
+		if (device.GetPress (SteamVR_Controller.ButtonMask.Touchpad)) {
+			Debug.Log ("You are holding the Touchpad");
+		}
+
+		if (device.GetPressDown (SteamVR_Controller.ButtonMask.Touchpad)) {
+			Debug.Log ("You pressed down the Touchpad");
+		}
+
+		if (device.GetPressUp (SteamVR_Controller.ButtonMask.Touchpad)) {
+			Debug.Log ("You let go of the Touchpad");
+			sphere.transform.position = new Vector3 (0, .5f, 0);
+			sphere.GetComponent<Rigidbody> ().velocity = Vector3.zero;
+			sphere.GetComponent<Rigidbody> ().angularVelocity = Vector3.zero;
+		}
+
+	}
+
+	void OnTriggerStay (Collider col) {
+		Debug.Log ("Controller collided with " + col.name);
+		if (device.GetTouch (SteamVR_Controller.ButtonMask.Trigger)) {
+			Debug.Log ("You are holding " + col.name);
+			col.attachedRigidbody.isKinematic = true;
+			col.gameObject.transform.SetParent (this.gameObject.transform);
+		}
+		if (device.GetTouchUp (SteamVR_Controller.ButtonMask.Trigger)) {
+			Debug.Log ("You release " + col.name);
+			col.attachedRigidbody.isKinematic = false;
+			col.gameObject.transform.SetParent (null);
+
+			tossObject (col.attachedRigidbody);
+		}
+	}
+
+	void tossObject(Rigidbody rigidbody) {
+		Transform origin = trackedObj.origin ? trackedObj.origin : trackedObj.transform.parent;
+		if (origin != null) {
+			rigidbody.velocity = origin.TransformVector (device.velocity);
+			rigidbody.angularVelocity = origin.TransformVector (device.angularVelocity);
+		} else {
+			rigidbody.velocity = device.velocity;
+			rigidbody.angularVelocity = device.angularVelocity;
+		}
 	}
 }
